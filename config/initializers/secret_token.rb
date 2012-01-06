de2c840f1304e05961e7732bf9bb3fb6a3966530
Bill Leeper <bleeper@onyxgs.com>
2012-01-06 15:02:24 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyGreatApp::Application.config.secret_token = 'b7bb7bb23973668663f94956435593980034d367b880ce160c26c5416bdb1e45c329ea5569d0dec038edc3c26e71cb4e7852ca53a79afb1171cde203139af0c6'
