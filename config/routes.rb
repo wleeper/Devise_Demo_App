535cb9ebf228ddcf17c7b276e5077bb31df6a56e
Bill Leeper <bleeper@onyxgs.com>
2012-01-09 09:08:05 -0700

    added some extra routes, but didn't use them, used the correct route to logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 MyGreatApp::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
